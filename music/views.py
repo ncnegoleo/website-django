3413dd90e837d73d16ca4d9ad0a8139df190ef42
Leonardo Soares <leonardo.nightcrawler@gmail.com>
2016-05-28 11:47:27 -0300

    escolha de musicas favoritas - redirecionando para mesma pagina

--- music/views.py
+++ music/views.py
@@ -1,14 +1,28 @@
-from django.http import HttpResponse
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
+
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    context = {'all_albums': all_albums}
+    return render(request, 'music/index.html', context)
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request,'music/detail.html', {
+            'album': album,
+            'error_message': "You did not select a valid song",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
